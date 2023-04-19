65987fd23c246c0aa62837f42eb964d778f313f8
PRASENJITSWIN10\prasenjits <Prasenjits@mindfiresolutions.com>
2023-04-19 22:34:17 +0530

    First commit

---
+++ InterviewPracticeSet/src/test/java/javaPractice/Capsule6/Ques3.java
@@ -0,0 +1,13 @@
+package javaPractice.Capsule6;
+
+import java.util.Scanner;
+
+public class Ques3 
+{
+	Scanner sc = new Scanner(System.in);
+	public static void main(String args[])
+	{
+		
+	}
+
+}
