086080798ad225c498571130a9338dc944fe03e2
PRASENJITSWIN10\prasenjits <Prasenjits@mindfiresolutions.com>
2023-05-07 23:38:49 +0530

    Adding some new codes

---
+++ InterviewPracticeSet/src/test/java/javaPractice/capsule1/Ques7.java
@@ -0,0 +1,20 @@
+package javaPractice.capsule1;
+
+public class Ques7 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Regrow"+1+2);//Regrow12
+		
+		System.out.println(1+2+"Regrow");//3Regrow
+		
+		System.out.println(1+2+"Regrow"+1+2);//3Regrow12
+		
+		System.out.println("Regrow"+1+2+"Tech");//Regrow12Tech
+		
+		System.out.println("Regrow"+(1+2)+"Tech");//Regrow3Tech
+		
+		System.out.println(1+2+"Regrow"+1+2+"Tech");//3Regrow12Tech
+		
+	}
+}
