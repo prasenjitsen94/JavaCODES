06774c82b5295f59664caef3b136a6ab177fe739
smriti94 <smritisaha19948@gmail.com>
2023-05-11 17:37:08 +0530

    adding inheritance

--- InterviewPracticeSet/src/test/java/smriti_practice/Practice1.java
+++ InterviewPracticeSet/src/test/java/smriti_practice/Practice1.java
@@ -1,21 +1,34 @@
 package smriti_practice;
 
 public class Practice1 {
-	
-	int no_of_car=2;
-	int no_of_house=5;
-	
+
+	int no_of_car;
+	int no_of_house;
+	int no_of_bike;
+
+
+	public Practice1(int cars, int houses, int bike)
+	{
+		no_of_car=cars;
+		no_of_house=houses;
+		no_of_bike=bike;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("I am parent class");
-		
+
 	}
-	
+
 	public void run()
 	{
 		System.out.println("Father runs slow");
+	}
+	
+	public void talk()
+	{
+		System.out.println("Father talks scramble");
 	}
 
 }
--- InterviewPracticeSet/src/test/java/smriti_practice/Practice2.java
+++ InterviewPracticeSet/src/test/java/smriti_practice/Practice2.java
@@ -2,26 +2,42 @@ package smriti_practice;
 
 public class Practice2 extends Practice1 {
 	int no_of_cycle;
-	int no_of_house;
+	int no_of_childhouse;
 	int no_of_car;
 
-	Practice2()
+	public Practice2(int cars, int houses, int cycle, int childhouse, int bike )
 	{
-		no_of_cycle=1;
-		no_of_house=3;
-		no_of_car= 1;
+		super(cars,houses,bike);
+		no_of_cycle=cycle;
+		no_of_childhouse=childhouse;
+		no_of_car=5;
 	}
 
 
 	public static void main(String[] args) {
-		System.out.println("I am child class");
-		Practice2 obj=new Practice2();
-		System.out.println(obj.no_of_car);
-		System.out.println(obj.no_of_cycle);
-		System.out.println(obj.no_of_house);
-		obj.run();
-		obj.callParent();
-		System.out.println(obj.no_of_car);
+		//System.out.println("I am child class");
+		Practice2 obj=new Practice2(3,4,2,1,7);
+//		System.out.println(obj.no_of_car);
+//		System.out.println(obj.no_of_cycle);
+//		System.out.println(obj.no_of_childhouse);
+//		obj.run();//
+//		obj.callParent();
+//		System.out.println(obj.no_of_car);
+		
+		//obj.talk();
+//		int x=15;
+//		System.out.println(x++ + x++ + ++x + 1 + x++);
+//		
+//		int y=3;
+//		System.out.println(++y + ++y + y++ + ++y + y++);
+		
+		System.out.println('a'+"Regrow"+1+7+3);
+		System.out.println('a'+ 'b' + "Regrow"+1+7+3);
+		System.out.println('a'+ 'b' + "Regrow"+1+7+(3+1));
+		System.out.println('a'+1+ "Regrow"+1+7+3+'A');
+		System.out.println('a'+"Regrow"+1+7+(3+'a'));
+
+		
 
 	}
 
@@ -35,6 +51,13 @@ public class Practice2 extends Practice1 {
 	public void run()
 	{
 		System.out.println("Child runs fast");
+	}
+	
+	public void talk()
+	{
+		System.out.println(super.no_of_bike);
+		super.talk();
+		System.out.println("child talks fluent");
 	}
 
 }
