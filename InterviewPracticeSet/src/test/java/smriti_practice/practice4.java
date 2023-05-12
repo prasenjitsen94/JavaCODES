55436ea02dedbe22df4b4b64aec878786db4ab1d
PRASENJITSWIN10\prasenjits <Prasenjits@mindfiresolutions.com>
2023-05-12 21:34:50 +0530

    Modified rimi's pattern code

--- InterviewPracticeSet/src/test/java/smriti_practice/practice4.java
+++ InterviewPracticeSet/src/test/java/smriti_practice/practice4.java
@@ -4,16 +4,22 @@ public class practice4 {
 
 	public static void main(String[] args) {
       
-		for(int i=3; i>=1; i--)
+		for(int i=1; i<=3; i++)
 		{
-			for(int j=1; j<=i; j++)
+			for(int j=3; j>=i; j--)
 			{
-				System.out.print("# ");
+				System.out.print(" ");
 			}
-			for(int k=3; k>=i; k--)
+			System.out.print("*");
+			for(int k=1; k<=i; k++)
 			{
-				System.out.print("*" + " #");	
+				if(i==1)
+					continue;
+				else
+					System.out.print("#");
 			}
+			if(i>1)
+				System.out.print("*");
 			
 		System.out.println();
 		}
