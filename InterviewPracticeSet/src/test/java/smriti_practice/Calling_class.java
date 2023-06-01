051b1be9d9f907a176ef06e5c4930fb730dac6fc
smriti94 <smritisaha19948@gmail.com>
2023-06-01 19:21:12 +0530

    adding

--- InterviewPracticeSet/src/test/java/smriti_practice/Calling_class.java
+++ InterviewPracticeSet/src/test/java/smriti_practice/Calling_class.java
@@ -1,15 +1,14 @@
 package smriti_practice;
 
-public class Calling_Class {
+public class Calling_class {
 
 	public static void main(String[] args) {
-		Private_Class obj=new Private_Class();
+		Private_class obj= new Private_class();
 		
-		System.out.println("Old Value : " + obj.getNum());
-		
-		obj.setNum(40);
-		
+		System.out.println("Old Value: " + obj.getNum());
+		obj.setNum(30);
 		System.out.println("New Value: " + obj.getNum());
+
 	}
 
 }
--- InterviewPracticeSet/src/test/java/smriti_practice/Private_class.java
+++ InterviewPracticeSet/src/test/java/smriti_practice/Private_class.java
@@ -1,20 +1,18 @@
 package smriti_practice;
 
-public class Private_Class {
+public class Private_class {
 	private int num=10;
 
 	public static void main(String[] args) {
 		
 	}
 	
-	public int getNum()
-	{
-		return num;
-	}
-	
-	public void setNum(int num)
-	{
-		this.num=num;
-	}
-
+		public int getNum()
+		{
+			return num;
+		}
+		public void setNum(int num)
+		{
+			this.num=num;
+		}
 }
