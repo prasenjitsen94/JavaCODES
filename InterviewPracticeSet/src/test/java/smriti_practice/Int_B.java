b24a138430d0f239ab59719d958092f165f3f82d
smriti94 <smritisaha19948@gmail.com>
2023-06-08 20:40:34 +0530

    abstraction code

---
+++ InterviewPracticeSet/src/test/java/smriti_practice/Int_B.java
@@ -0,0 +1,8 @@
+package smriti_practice;
+
+public interface Int_B {
+	
+	public abstract void m1_B();
+	public abstract void m2_B();
+
+}
---
+++ InterviewPracticeSet/src/test/java/smriti_practice/Interface_A.java
@@ -0,0 +1,9 @@
+package smriti_practice;
+
+public interface Interface_A {
+	
+	public abstract void m1();
+	public abstract void m2();
+	public abstract void m3();
+
+}
