4e9c4c55fc6802dbbd4693fdcae00926a6df1fad
smriti94 <smritisaha19948@gmail.com>
2023-06-18 21:55:58 +0530

    adding constructor chaining

---
+++ InterviewPracticeSet/src/test/java/smriti_practice/Parent.java
@@ -0,0 +1,22 @@
+package smriti_practice;
+
+public class Parent {
+	
+	Parent()
+	{
+		System.out.println("No-arguments constructor of Parent class");
+	}
+	
+	Parent(int x)
+	{
+		this();
+		System.out.println("Calling parametrized constructor of parent");
+	}
+	
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
